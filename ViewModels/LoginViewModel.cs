a5eecdb384b41f90230e16cf5e3df60dd30aba04
User <User@LAB2-2>
2024-03-18 14:07:33 +0200

    finished the login (needs a fix about the tap Command)

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using TriviaAppClean.Models;
@@ -17,12 +18,158 @@ namespace TriviaAppClean.ViewModels
         private RegisterView register;
         public LoginViewModel(TriviaWebAPIProxy service, RegisterView register) 
         {
-            this.register = register;
             InServerCall = false;
             this.triviaService = service;
             this.LoginCommand = new Command(OnLogin);
-            this.TapCommand = new Command(Tap);
+            this.NameError = "This is a required field";
+            this.PasswordError = "This is a required field";
+            this.EmailError = "This is a required field";
+
+            //this.register = register;
+            //InServerCall = false;
+            //this.triviaService = service;
+            //this.LoginCommand = new Command(OnLogin);
+            //this.TapCommand = new Command(Tap);
         }
+
+        #region Name
+        private bool showNameError;
+
+        public bool ShowNameError
+        {
+            get => showNameError;
+            set
+            {
+                showNameError = value;
+                OnPropertyChanged("ShowNameError");
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                ValidateName();
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string nameError;
+
+        public string NameError
+        {
+            get => nameError;
+            set
+            {
+                nameError = value;
+                OnPropertyChanged("NameError");
+            }
+        }
+
+        private void ValidateName()
+        {
+            this.ShowNameError = string.IsNullOrEmpty(Name);
+        }
+        #endregion
+
+        #region password
+        private bool showPasswordError;
+
+        public bool ShowPasswordError
+        {
+            get => showPasswordError;
+            set
+            {
+                showPasswordError = value;
+                OnPropertyChanged("ShowPasswordError");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                ValidateName();
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private string passwordError;
+
+        public string PasswordError
+        {
+            get => passwordError;
+            set
+            {
+                passwordError = value;
+                OnPropertyChanged("PasswordError");
+            }
+        }
+
+        private void ValidatePassword()
+        {
+            this.ShowPasswordError = string.IsNullOrEmpty(Password);
+        }
+        #endregion
+
+
+        #region email
+        private bool showEmailError;
+
+        public bool ShowEmailError
+        {
+            get => showNameError;
+            set
+            {
+                showNameError = value;
+                OnPropertyChanged("ShowEmailError");
+            }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                ValidateEmail();
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string emailError;
+
+        public string EmailError
+        {
+            get => emailError;
+            set
+            {
+                emailError = value;
+                OnPropertyChanged("EmailError");
+            }
+        }
+
+        private void ValidateEmail()
+        {
+            string email = Email;
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            this.ShowNameError = !match.Success;
+        }
+        #endregion
+
+
+
 
         public ICommand LoginCommand { get; set; }
         private async void OnLogin()
@@ -69,11 +216,12 @@ namespace TriviaAppClean.ViewModels
                 return !this.InServerCall;
             }
         }
-        public Command TapCommand { get; set; }
-        private async void Tap() 
-        {
-            await Application.Current.MainPage.Navigation.PushAsync(register);  
-        }
+        //public Command TapCommand { get; set; }
+        //private async void Tap() 
+        //{
+        //    await Application.Current.MainPage.Navigation.PushAsync(register);  
+        //} 
+        
 
     }
 }
---
+++ ViewModels/UserListViewModel.cs
@@ -0,0 +1,39 @@
+﻿//using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TriviaAppClean.Models;
+using TriviaAppClean.Services;
+
+namespace TriviaAppClean.ViewModels
+{
+    internal class UserListViewModel : ViewModelBase
+    {
+        private Service service;
+        private ObservableCollection<User> players;
+        public ObservableCollection<User> Players { get { return players; } set { players = value; OnPropertyChanged(); } }
+        private bool isRefreshing;
+        public bool IsRefreshing { get { return isRefreshing; } set { isRefreshing = value; OnPropertyChanged(); } }
+        private Color errorColor;
+        public Color ErrorColor { get { return errorColor; } set { errorColor = value; OnPropertyChanged(); } }
+        private string errorMsg;
+        public string ErrorMsg { get { return errorMsg; } set { this.errorMsg = value; OnPropertyChanged(); } }
+        private string emailEntry;
+        public string EmailEntry { get { return emailEntry; } set { this.emailEntry = value; OnPropertyChanged(); ((Command)AddPlayerCommand).ChangeCanExecute(); } }
+        private string passwordEntry;
+        public string PasswordEntry { get { return passwordEntry; } set { this.passwordEntry = value; OnPropertyChanged(); ((Command)AddPlayerCommand).ChangeCanExecute(); } }
+        private string playerNameEntry;
+        public string PlayerNameEntry { get { return playerNameEntry; } set { this.playerNameEntry = value; OnPropertyChanged(); ((Command)AddPlayerCommand).ChangeCanExecute(); } }
+        public ICommand AddPlayerCommand { get; private set; }
+        public ICommand RefreshCommand { get; private set; }
+        public ICommand SortByRankCommand { get; private set; }
+        public ICommand ResetPlayerPointsCommand { get; private set; }
+        public ICommand RemovePlayerCommand { get; private set; }
+
+
+    }
+}
