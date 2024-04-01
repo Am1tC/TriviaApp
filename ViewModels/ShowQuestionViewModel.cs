9b4ff80fda735b2680dc0d2c1f504961533f796e
AC1231231 <amitchacham1@gmail.com>
2024-04-01 14:03:45 +0300

    added partial ashow question

--- ViewModels/ShowQuestionViewModel.cs
+++ ViewModels/ShowQuestionViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TriviaAppClean.Models;
 using TriviaAppClean.Services;
 
 namespace TriviaAppClean.ViewModels
@@ -11,6 +12,8 @@ namespace TriviaAppClean.ViewModels
     public class ShowQuestionViewModel : ViewModelBase
     {
         string searchBar;
+        private TriviaWebAPIProxy triviaService;
+        private List<AmericanQuestion> q;
         string SerachBar
         {
             get
@@ -38,11 +41,40 @@ namespace TriviaAppClean.ViewModels
 
         private void OnSearchTextChanged()
         {
-          
+            ObservableCollection<AmericanQuestion> tmp = new ObservableCollection<AmericanQuestion>();
+            ReadQuestions();
+            if (!String.IsNullOrEmpty(SearchText))
+            {
+                foreach (AmericanQuestion qu in this.q)
+                {
+                    if (qu.QText.IndexOf(searchBar, StringComparison.OrdinalIgnoreCase) == -1)
+                    {
+                        tmp.Add(qu);
+                    }
+                }
+
+                foreach (AmericanQuestion que in tmp)
+                {
+                    if (!this.Questions.Contains(que))
+                    {
+                        this.Questions.Remove(que);
+                    }
+                }
+            }
+
+
+
+
+        }
+        private void ReadQuestions()
+        {
+
+            this.Questions = new ObservableCollection<AmericanQuestion>(questions);
+
         }
 
-        ObservableCollection<Questions> questions;
-        ObservableCollection<Questions> Questions
+        ObservableCollection<AmericanQuestion> questions;
+        ObservableCollection<AmericanQuestion> Questions
         {
             get
             {
